8c524965a2acd47fc9f6414173dfda8045304d5f
Bryan D. Payne <bdpayne@acm.org>
2013-04-05 00:02:52 +0000

    switch to apache 2.0 license

--- tools/pyvmi/pyvmi/__about__.py
+++ tools/pyvmi/pyvmi/__about__.py
@@ -1,25 +1,24 @@
 # pylint: disable=C0301
 """
-PyVMI is a python wrapper around the LibVMI Library, with some added niceties
-to make it more suitable for python programmers.  For more information on
-LibVMI and PyVMI, see http://www.libvmi.com.
+PyVMI is a python language wrapper for the LibVMI Library. The LibVMI Library
+is an introspection library that simplifies access to memory in a target
+virtual machine or in a file containing a dump of a system's physical memory.
 
 Author: Bryan D. Payne (bdpayne@acm.org)
 
-This file is part of PyVMI.
+Copyright 2013 Bryan D. Payne
 
-LibVMI is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-LibVMI is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-License for more details.
+       http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU Lesser General Public License
-along with LibVMI.  If not, see <http://www.gnu.org/licenses/>.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 from __future__ import absolute_import
 from __future__ import division
